9675cbecad2cd096b239c0e2cc2cf34517599246
Marvin <652141046@qq.com>
2017-12-05 10:30:23 +0800

    打印类成员变量和虚指针的偏移地址，另外验证了两个char之间没有内存对齐

--- ch03/MemberPtr.cc
+++ ch03/MemberPtr.cc
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -11,11 +12,15 @@ virtual void fun() {};
 int main() {
     class A a;
     cout << "sieof char: "<< sizeof(char) << endl
-        << "sieof ptr: "<< sizeof(char*) << endl
+        << "sieof ptr: "<< sizeof(void*) << endl
         << "sieof A: "<< sizeof(A) << endl;
-    cout << "a_: " << &A::a_ << endl <<
-        "b_: " << &A::b_ << endl <<
-        "fun(): " << &A::fun << endl;
+    //cout << "a_: " << &A::a_ << endl
+        //<< "b_: " << &A::b_ << endl
+        //<< "fun(): " << &A::fun << endl;
+
+    printf("a_: %p\n", (&A::a_));
+    printf("b_: %p\n", (&A::b_));
+    printf("fun: %p\n", (&A::fun));
 
     return 0;
 }
