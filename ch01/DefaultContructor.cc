d7d656011d337ff8797f4c2c7351c038d1a2c460
Marvin <652141046@qq.com>
2017-12-07 15:11:03 +0800

    验证何时编译器会合成默认构造函数

---
+++ ch01/DefaultContructor.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class A {
+public:
+    int a_;
+    char b_;
+};
+
+class B {
+public:
+    int a_ = 10;
+    char b_ = 'b';
+};
+
+class C {
+public:
+    int a_ = 10;
+    char b_ = 'b';
+    virtual int fun() { return 0; }
+};
+
+int main() {
+    A a;
+    cout << "class A: " << a.a_ << " " << a.b_ << endl;
+    B b;
+    cout << "class B: " << b.a_ << " " << b.b_ << endl;
+    C c;
+    cout << "class B: " << c.a_ << " " << c.b_ << endl;
+}
